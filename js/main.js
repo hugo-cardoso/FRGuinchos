477be370cb3cd9d40a4c428b831b0a0ec45ebba2
tunadao1 <hugorafaelmc@gmail.com>
2017-06-13 03:00:10 -0300

    Rotas

--- js/main.js
+++ js/main.js
@@ -24,49 +24,75 @@ $(".hamburger, aside a").click(function(){
 
 // Maps
 
-var itaquera = {
-  lat: "-23.5397307",
-  long: "-46.4480022"
-}
-
 function deg2rad(deg) {
   return deg * (Math.PI/180)
 }
 
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(showPosition);
+    navigator.geolocation.getCurrentPosition(function(position){
+
+      aqui = {
+        lat: Number(position.coords.latitude),
+        lng: Number(position.coords.longitude)
+      };
+
+      center(aqui.lat,aqui.lng);
+
+    });
   } else {
     console.log("Geolocation is not supported by this browser.");
   }
 }
 
-function showPosition(position) {
-  aqui = {
-    lat: position.coords.latitude,
-    long: position.coords.longitude
-  };
+function center(lat,lng) {
+
+  getAdress(lat,lng);
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: new google.maps.LatLng(parseFloat(lat),parseFloat(lng))
+  });
+  var marker = new google.maps.Marker({
+    position: aqui,
+    map: map
+  });
+  directionsService = new google.maps.DirectionsService;
+  directionsDisplay = new google.maps.DirectionsRenderer;
+  directionsDisplay.setMap(map);
+
 }
 
-function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
-  var R = 6371; // Radius of the earth in km
-  var dLat = deg2rad(lat2-lat1);  // deg2rad below
-  var dLon = deg2rad(lon2-lon1);
-  var a =
-  Math.sin(dLat/2) * Math.sin(dLat/2) +
-  Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
-  Math.sin(dLon/2) * Math.sin(dLon/2)
-  ;
-  var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-  var d = R * c; // Distance in km
+function getAdress(lat,lng){
 
-  getPriceKm(d);
+  $.get( "https://maps.googleapis.com/maps/api/geocode/json?latlng=" + lat + "," + lng + "&sensor=false", function( data ) {
+    console.log(data);
+    $("#retirada").val(data.results[0].formatted_address);
+  });
+
+}
+
+
+function getDistanceFromLatLonInKm(lat1,lon1,lat2,lon2) {
+var R = 6371; // Radius of the earth in km
+var dLat = deg2rad(lat2-lat1);  // deg2rad below
+var dLon = deg2rad(lon2-lon1);
+var a =
+Math.sin(dLat/2) * Math.sin(dLat/2) +
+Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) *
+Math.sin(dLon/2) * Math.sin(dLon/2)
+;
+var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+var d = R * c; // Distance in km
+var d = parseInt(d);
+
+console.log(d);
+return d;
 
 }
 
 function getPriceKm(km){
 
-  var distante = parseInt(km);
+  var distante = parseInt(km  / 1000);
   var price = 120 + (2 * distante);
 
   console.log("Preço: R$" + price)
@@ -74,17 +100,67 @@ function getPriceKm(km){
 
 }
 
+function calculateAndDisplayRoute(directionsService, directionsDisplay) {
+
+  var destino = document.getElementById("destino").value;
+  var retirada = document.getElementById("retirada").value;
+
+  directionsService.route({
+    origin: "itaquera",
+    destination: destino,
+    travelMode: 'DRIVING',
+    waypoints: [
+      {
+        location: new google.maps.LatLng(parseFloat(aqui.lat),parseFloat(aqui.lng))
+      }
+    ]
+  }, function(response, status) {
+
+    if (status === 'OK') {
+      console.log(response);
+      directionsDisplay.setDirections(response);
+
+      var totalKm = 0;
+
+      for (var i = 0; i < response.routes[0].legs.length; i++) {
+        
+        totalKm += response.routes[0].legs[i].distance.value;
+
+      }
+
+      console.log(totalKm);
+
+      getPriceKm(totalKm);
+
+    } 
+
+
+  });
+}
+
 $("#orcar").click(function(){
 
   var geocoder = new google.maps.Geocoder();
   var destino = document.getElementById("destino").value;
-  var destino = destino.replace(" ","%");
+  var retirada = document.getElementById("retirada").value;
 
-  $.get( "https://maps.googleapis.com/maps/api/geocode/json?address=" + destino + "&key=AIzaSyAgXBzHEtFvOvuuBktjuCH8r_e1_CC6LHU", function( data ) {
-    console.log(data);
-    $("#destino").val(data.results[0].formatted_address);
-    destinoCoord = data.results[0].geometry.location;
-    getDistanceFromLatLonInKm(aqui.lat,aqui.long,destinoCoord.lat,destinoCoord.lng);
+  $.get( "https://maps.googleapis.com/maps/api/geocode/json?address=" + retirada + "&key=AIzaSyAgXBzHEtFvOvuuBktjuCH8r_e1_CC6LHU", function( data ) {
+
+    var lat = data.results[0].geometry.location.lat;
+    var lng = data.results[0].geometry.location.lng;
+
+    aqui = {lat: lat, lng: lng};
+
+    $.get( "https://maps.googleapis.com/maps/api/geocode/json?address=" + destino + "&key=AIzaSyAgXBzHEtFvOvuuBktjuCH8r_e1_CC6LHU", function( data2 ) {
+        
+        $("#destino").val(data2.results[0].formatted_address);
+        
+        destinoCoord = data2.results[0].geometry.location;
+
+        calculateAndDisplayRoute(directionsService, directionsDisplay);
+
+      });
+
   });
 
 });
@@ -95,4 +171,11 @@ $(".centerMap").click(function(){
 
 })
 
-getLocation();
+$(function(){
+ $(window).load(function(){
+
+  getLocation();
+
+
+});
+});
