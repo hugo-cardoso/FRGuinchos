e6411d508f5470a3bf2b8698c1963f8d53acdb99
tunadao1 <hugorafaelmc@gmail.com>
2017-06-14 18:36:23 -0300

    Sidebar Itens and loading

--- js/main.js
+++ js/main.js
@@ -20,18 +20,58 @@ $(".menu-icon, .aside-bg").click(function(){
 
 });
 
-function createMessage(msg){
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
 
-  var elm = $(".message");
+    $(".aside-bg").removeClass("active");
+    $("aside").removeClass("active");
 
-  $(elm).addClass("active").promise().done(function(){
-    $(elm).html(msg);
-    setTimeout(function(){
-      $(elm).removeClass("active");
-    }, 5000);
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+      ) {
+      // Figure out element to scroll to
+    var target = $(this.hash);
+    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top - 50
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
   });
 
-}
+  function createMessage(msg){
+
+    var elm = $(".message");
+
+    $(elm).addClass("active").promise().done(function(){
+      $(elm).html(msg);
+      setTimeout(function(){
+        $(elm).removeClass("active");
+      }, 5000);
+    });
+
+  }
 
 // Maps
 
