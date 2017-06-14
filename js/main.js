384d2d863db69b3f81176bfc0886ace22d4b4eaa
Hugo Rafael de Moraes Cardoso <HugoRafael@Idealista.local>
2017-06-13 22:18:30 -0300

    Autocomplete

--- js/main.js
+++ js/main.js
@@ -274,11 +274,43 @@ $(".centerMap").click(function(){
 
 })
 
-$(function(){
-  $(window).load(function(){
+// AutoComplete
 
-    getLocation();
+function initAutocomplete() {
+  // Create the autocomplete object, restricting the search to geographical
+  // location types.
+  autocomplete = new google.maps.places.Autocomplete(
+    /** @type {!HTMLInputElement} */(document.getElementById('retirada')),
+    {types: ['geocode']});
+
+    autocomplete = new google.maps.places.Autocomplete(
+      /** @type {!HTMLInputElement} */(document.getElementById('destino')),
+      {types: ['geocode']});
+
+    }
+
+    function geolocate() {
+      if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(function(position) {
+          var geolocation = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+          };
+          var circle = new google.maps.Circle({
+            center: geolocation,
+            radius: position.coords.accuracy
+          });
+          autocomplete.setBounds(circle.getBounds());
+        });
+      }
+    }
+
+    $(function(){
+      $(window).load(function(){
+
+        getLocation();
+        initAutocomplete();
 
 
-  });
-});
+      });
+    });
